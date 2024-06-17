40c7d5b14c6e2dcbb162a90b859267f3a41bfaee
Graileth Pereira <grailethpereirasanm@gmail.com>
2024-06-17 15:40:30 -0400

    correcciones de karen

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { ItemListC } from "./components/ItemListC"
 import  NotFound  from "./components/NotFound"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import "./css/main.css"
-import Item from "./components/Item"
 import { ItemDetailContainer } from "./components/ItemDetailContainer"
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
       <Routes>
         <Route path="/" element={<ItemListC />}/>
         <Route path="/category/:categoryId" element={<ItemListC />} />
-        <Route path="/Item:id" element={<Item />}/>
         <Route path="/item/:itemId" element={<ItemDetailContainer />}/>
         <Route path="*" element={<NotFound />} />
       </Routes>
